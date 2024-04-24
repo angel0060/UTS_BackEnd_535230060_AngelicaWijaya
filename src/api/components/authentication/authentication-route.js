8ced80ea0fb6697f03025ab60610b87fafedf47d
angel0060 <angelica.535230060@stu.untar.ac.id>
2024-04-24 23:27:25 +0700

    tambah limit di authentication-route login

--- src/api/components/authentication/authentication-route.js
+++ src/api/components/authentication/authentication-route.js
@@ -3,15 +3,38 @@ const express = require('express');
 const authenticationControllers = require('./authentication-controller');
 const authenticationValidators = require('./authentication-validator');
 const celebrate = require('../../../core/celebrate-wrappers');
+const { rateLimit } = require('express-rate-limit');
+const { errorResponder, errorTypes } = require('../../../core/errors');
 
 const route = express.Router();
 
 module.exports = (app) => {
-  app.use('/authentication', route);
+  const error = errorResponder(
+    errorTypes.FORBIDDEN,
+    'Too many failed login attempts'
+  );
+
+  const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 5,
+    message: {
+      error,
+      message: 'Too many failed login attempts, try again in 30 minutes',
+    },
+    Headers: true,
+    skipSuccessfulRequests: true,
+  });
+
+  app.use('/authentication', limiter, route);
 
   route.post(
     '/login',
     celebrate(authenticationValidators.login),
-    authenticationControllers.login
+    authenticationControllers.login,
+    async () => {
+      if (authenticationControllers.login != error) {
+        limiter.resetKey();
+      }
+    }
   );
 };
