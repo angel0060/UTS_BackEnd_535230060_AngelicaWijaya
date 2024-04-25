3eed15ad2d18757774d3ff64a0958fc90e75fbf5
angel0060 <angelica.535230060@stu.untar.ac.id>
2024-04-25 22:01:28 +0700

    uninstall express-rate-limit, hapus rate limit di authentication-route

--- src/api/components/authentication/authentication-route.js
+++ src/api/components/authentication/authentication-route.js
@@ -3,38 +3,15 @@ const express = require('express');
 const authenticationControllers = require('./authentication-controller');
 const authenticationValidators = require('./authentication-validator');
 const celebrate = require('../../../core/celebrate-wrappers');
-const { rateLimit } = require('express-rate-limit');
-const { errorResponder, errorTypes } = require('../../../core/errors');
 
 const route = express.Router();
 
 module.exports = (app) => {
-  const error = errorResponder(
-    errorTypes.FORBIDDEN,
-    'Too many failed login attempts'
-  );
-
-  const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000,
-    max: 5,
-    message: {
-      error,
-      message: 'Too many failed login attempts, try again in 30 minutes',
-    },
-    Headers: true,
-    skipSuccessfulRequests: true,
-  });
-
-  app.use('/authentication', limiter, route);
+  app.use('/authentication', route);
 
   route.post(
     '/login',
     celebrate(authenticationValidators.login),
-    authenticationControllers.login,
-    async () => {
-      if (authenticationControllers.login != error) {
-        limiter.resetKey();
-      }
-    }
+    authenticationControllers.login
   );
 };
